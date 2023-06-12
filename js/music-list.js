8b034dbe6f004dac8b41d72842a73652e6c1317f
Roni <132615782+Rooni0@users.noreply.github.com>
2023-06-12 16:46:59 -0500

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,86 @@
+let allMusic = [
+  {
+    name: "Cha Cha",
+    artist: "Freddie Dredd",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Dark Red",
+    artist: "Steve Lacy",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Heart To heart",
+    artist: "Mac DeMarco",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lakim - A pimp Named Slickback",
+    artist: "Lakim",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Oh Klahoma",
+    artist: "Jack Stauber",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Princess Bubblegum",
+    artist: "ilyTOMMY",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Gypsy Woman (slowed)",
+    artist: "haraguchi",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name:"Freddie Dredd - Wrath",
+    artist: "Freddie",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name:"Just The Two Of Us",
+    artist:"Two",
+    img:"music-9",
+    src:"music-9"
+  },
+  {
+    name:"Messages Fromthe Stars",
+   artist:"The RAH Band",
+   img:"music-10",
+   src:"music-10"
+  },
+  {
+   name:"MAXIMUM CARNAGE",
+   artist:"SLAVNIY PRIHOD",
+   img:"music-11",
+   src:"music-11"
+  },
+  {
+   name:"MurderCaust",
+   artist:"Lord Lorenz",
+   img:"music-12",
+   src:"music-12"
+  },
+  {
+    name:"Fighting Gold",
+    artist:"Jojo's bizarre adventure",
+    img:"music-13",
+    src:"music-13"
+  },
+  {
+    name:"Day 'N' Nite",
+    artist:"Kid Cudi",
+    img:"music-14",
+    src:"music-14"
+  },
+];
